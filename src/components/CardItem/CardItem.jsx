41d78f373a9a16beab19b74ece92a95d1c03a89c
tymur-lvov <pmtl753@gmail.com>
2024-06-12 22:21:48 +0300

    Updated Catalog

--- src/components/CardItem/CardItem.jsx
+++ src/components/CardItem/CardItem.jsx
@@ -1,3 +1,7 @@
+import icons from "../../assets/icons/icons.svg";
+
+import css from "./CardItem.module.css";
+
 const CardItem = ({ auto }) => {
   const {
     img,
@@ -14,7 +18,32 @@ const CardItem = ({ auto }) => {
 
   return (
     <>
-      <img src={img} alt={`${make} ${model} ${year}`} />
+      <div className={css.thumb}>
+        <img src={img} alt={`${make} ${model}`} />
+        <svg className={css.icon} height={18} width={18}>
+          <use href={`${icons}#icon-heart`}></use>
+        </svg>
+      </div>
+      <div className={css.content_wrapper}>
+        <div className={css.title_wrapper}>
+          <h3 className={css.title}>
+            {make} <span className={css.accent}>{model}</span>, {year}
+          </h3>
+          <span className={css.price}>{rentalPrice}</span>
+        </div>
+        <ul className={css.list}>
+          <li>{address}</li>
+          <li>{address}</li>
+          <li>{rentalCompany}</li>
+          <li>{type}</li>
+          <li>{model}</li>
+          <li>{id}</li>
+          <li>{accessories[0]}</li>
+        </ul>
+        <button className={css.button} type="button">
+          Learn more
+        </button>
+      </div>
     </>
   );
 };
