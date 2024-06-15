142ecdddf394cfbdb606415be17f3f7c07a6f3fb
tymur-lvov <pmtl753@gmail.com>
2024-06-15 16:09:53 +0300

    Updated CatalogPage

--- src/pages/CatalogPage/CatalogPage.jsx
+++ src/pages/CatalogPage/CatalogPage.jsx
@@ -13,26 +13,23 @@ import {
   selectCatalog,
   selectIsLimit,
   selectIsLoading,
-  selectRefCatalog,
 } from "../../redux/auto/autosSelectors";
 
 import css from "./CatalogPage.module.css";
 
 const CatalogPage = () => {
   const catalog = useSelector(selectCatalog);
-  const refCatalog = useSelector(selectRefCatalog);
   const isLimit = useSelector(selectIsLimit);
   const isLoading = useSelector(selectIsLoading);
   const currentPage = useRef(1);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    if (catalog.length === 0 || refCatalog.length === 0) {
-      dispatch(fetchCatalogThunk());
-    }
-  }, [catalog.length, refCatalog.length, dispatch]);
+    dispatch(fetchCatalogThunk());
+  }, [dispatch]);
 
   const handleLoadMoreClick = () => {
+    currentPage.current++;
     dispatch(fetchMoreAutosThunk(currentPage.current));
   };
 
@@ -43,12 +40,11 @@ const CatalogPage = () => {
       {!isLoading && (
         <>
           <CardsList catalog={catalog} />
-          {!isLimit ||
-            (catalog.length > 12 && (
-              <button className={css.button} onClick={handleLoadMoreClick}>
-                Load more
-              </button>
-            ))}
+          {!isLimit && (
+            <button className={css.button} onClick={handleLoadMoreClick}>
+              Load more
+            </button>
+          )}
         </>
       )}
     </>
